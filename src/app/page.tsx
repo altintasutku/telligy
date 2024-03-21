81526b1f56bef92931bebd95696f14c2699ae383
altintasutku <altintasutku@gmail.com>
2024-03-21 17:03:02 +0300

    ss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { pdfjs } from "react-pdf";
 import { useState } from "react";
 import { Document, Page } from "react-pdf";
+import { Button } from "@/components/ui/button";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
@@ -22,20 +23,18 @@ export default function Home() {
       <p>
         Page {pageNumber} of {numPages}
       </p>
-      <button
+      <Button
         disabled={pageNumber === 1}
         onClick={() => setPageNumber((prev) => prev - 1)}
-        className="bg-slate-600 disabled:bg-slate-200 hover:bg-slate-800 transition-all px-5 py-2 text-white mr-5 rounded-md"
       >
         prev
-      </button>
-      <button
+      </Button>
+      <Button
         disabled={pageNumber === numPages}
         onClick={() => setPageNumber((prev) => prev + 1)}
-        className="bg-slate-600 disabled:bg-slate-200 hover:bg-slate-800 transition-all px-5 py-2 text-white mr-5 rounded-md"
       >
         next
-      </button>
+      </Button>
     </div>
   );
 }
